b098ac8a5b99d73895dcd3a9a4b6fadaef90b415
unknown <julian.pena-m@labinfo.is.escuelaing.edu.co>
2022-06-23 10:26:29 -0500

    First Commit

---
+++ src/main/java/edu/escuelaing/arsw/App.java
@@ -0,0 +1,50 @@
+package edu.escuelaing.arsw;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@RestController
+public class App {
+
+    @Resource
+    private HttpServletRequest request;
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @RequestMapping(value = "/status", method = RequestMethod.GET, produces = "application/json")
+    public String status() {
+        sessionManagement();
+        String name = (String) request.getSession().getAttribute("name");
+        return "{\"status\":\"Greetings from Spring Boot. "
+                + name + " , "
+                + java.time.LocalDate.now() + ", "
+                + java.time.LocalTime.now()
+                + ". " + "The server is Runnig!\"}";
+    }
+
+    @GetMapping("/setname")
+    public String setName(@RequestParam(value = "name", defaultValue = "Anónimo") String name) {
+        request.getSession().setAttribute("name", name);
+        return String.format("Hello %s!", name);
+    }
+
+    public void sessionManagement() {
+        System.out.println(request.getSession(true).getId());
+    }
+
+}
